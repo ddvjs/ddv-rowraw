74737f23c7fc52dcfe0c23c8567333a6234338c1
yuchonghua <yuchonghua@163.com>
2017-02-05 13:57:21 +0800

    add rowraw code

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,7 @@ module.exports = {
     sourceType: 'module'
   },
   env: {
+    browser: true,
     node: true
   },
   // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
